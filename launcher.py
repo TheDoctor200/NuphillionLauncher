b3305b387662b5de540a6cfecd56d20ffba88bf2
Richard <grafdracula091@gmail.com>
2025-05-18 01:04:21 +0200

    powershell console hidden

--- launcher.py
+++ launcher.py
@@ -152,7 +152,14 @@ def get_aumid(app_name_filter="Halo Wars 2"):
     ps_command = f'''
     Get-StartApps | Where-Object {{$_.Name -like "*{app_name_filter}*"}} | Select-Object -ExpandProperty AppID
     '''
-    result = subprocess.run(["powershell", "-Command", ps_command], capture_output=True, text=True)
+    # Hide the PowerShell window by using creationflags
+    CREATE_NO_WINDOW = 0x08000000
+    result = subprocess.run(
+        ["powershell", "-Command", ps_command],
+        capture_output=True,
+        text=True,
+        creationflags=CREATE_NO_WINDOW
+    )
     aumids = result.stdout.strip().splitlines()
 
     if not aumids:
